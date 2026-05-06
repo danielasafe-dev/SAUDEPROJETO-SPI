830c7bfa9af9d477a67347866a3120ee7275a2df
Daniel <danielasafesilva@hotmail.com>
2026-05-06 18:31:13 -0300

    senha

--- backend-dotnet/src/SPI.Infraestrutura/Email/OpcoesConviteSenha.cs
+++ backend-dotnet/src/SPI.Infraestrutura/Email/OpcoesConviteSenha.cs
@@ -4,7 +4,7 @@ public sealed class PasswordInviteOptions
 {
     public const string SectionName = "PasswordInvite";
 
-    public string FrontendBaseUrl { get; init; } = "http://localhost:5173";
+    public string FrontendBaseUrl { get; init; } = "http://localhost:5174";
     public int ExpireMinutes { get; init; } = 1440;
 }
 
